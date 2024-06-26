49b15279c928d351c49a118b76ea3da9a9df1be2
Shaurya144 <chundawatshaurya@gmail.com>
2024-06-26 19:45:05 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    // Concatenation is the process of joing two or more strings together to form
+    // one string
+
+    String firstName = "FirstName";
+    String lastName = "LastName";
+    System.out.println(firstName + " " + lastName);
+
+    // However with number it can be a bit weird
+
+    int x = 5;
+    int y = 10;
+    System.out.println(x + y); // 15
+
+    String StringX = "5";
+    String StringY = "10";
+    System.out.println(StringX + StringY); // "510"
+
+    /*
+     * Escape character Result Description
+     * \' for ' Single quote
+     * \"	for " Double quote
+     * \\ for \ Backslash
+     */
+  }
+}
